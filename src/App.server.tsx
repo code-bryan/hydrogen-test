34df4af3f7c46363598a68456d32945252b7c88c
code.bryan <brya0196@gmail.com>
2023-03-19 18:36:53 -0400

    feat: include kanvas core

--- src/App.server.tsx
+++ src/App.server.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import renderHydrogen from '@shopify/hydrogen/entry-server';
 import {Router, FileRoutes, ShopifyProvider} from '@shopify/hydrogen';
 import {Suspense} from 'react';
+import KanvasCore from '@kanvas/core';
+
+const {} = new KanvasCore({
+  key: '',
+  url: '',
+});
 
 function App() {
   return (
